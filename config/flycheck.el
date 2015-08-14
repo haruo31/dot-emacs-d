90572d76799c9573c24c8a3ff0d6ef8870245ac5
Haruo Kinoshita <haruo31@underthetree.jp>
2015-08-14 19:46:35 +0900

    added extra flycheck options.

--- config/flycheck.el
+++ config/flycheck.el
@@ -1,6 +1,17 @@
 (unless (package-installed-p 'flycheck)
   (package-refresh-contents) (package-install 'flycheck))
 
+(unless (package-installed-p 'flycheck-pyflakes)
+  (package-refresh-contents) (package-install 'flycheck-pyflakes))
+(unless (package-installed-p 'flycheck-pos-tip)
+  (package-refresh-contents) (package-install 'flycheck-pos-tip))
+
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-hook 'python-mode-hook #'flycheck-mode)
+;; epylint へパスが通っていないとシンタックスチェックが働かない
+;; (setq exec-path (cons "/usr/local/bin" exec-path))
+
+(eval-after-load 'flycheck
+  '(custom-set-variables
+   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
