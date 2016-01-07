43228f13e45b8949aa5e5cdedabe2442b4abb4c2
Haruo Kinoshita <haruo31@underthetree.jp>
2016-01-08 05:04:24 +0900

    added company-anaconda

--- config/company.el
+++ config/company.el
@@ -1,6 +1,13 @@
 (unless (package-installed-p 'company)
   (package-refresh-contents) (package-install 'company))
+(unless (package-installed-p 'company-anaconda)
+  (package-refresh-contents) (package-install 'company-anaconda))
 
 ;; company-mode
 ;; (add-hook 'after-init-hook 'global-company-mode)
 (global-company-mode 1)
+
+(eval-after-load "company"
+ '(progn
+   (add-to-list 'company-backends 'company-anaconda)))
+(add-hook 'python-mode-hook 'anaconda-mode)
